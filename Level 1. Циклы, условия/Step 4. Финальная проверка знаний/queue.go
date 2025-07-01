d2b7697061e8e3f1c5f3c00a3a4e4718039dac13
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-02 00:08:45 +0700

    error task

--- Level 1. Циклы, условия/Step 4. Финальная проверка знаний/queue.go
+++ Level 1. Циклы, условия/Step 4. Финальная проверка знаний/queue.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main(){
 	var place1,	place2, place3, place4, place5 string
-
+	fmt.Scan(place1)
+	fmt.Scan(place2)
+	fmt.Scan(place3)
+	fmt.Scan(place4)
+	fmt.Scan(place5)
 	for{
 		var input string
 		fmt.Scan(&input)
@@ -21,6 +25,30 @@ func main(){
 			fmt.Println(3, place3)
 			fmt.Println(4, place4)
 			fmt.Println(5, place5)
+			continue
+		}
+
+		if input == "количество"{
+			occupied := 0
+
+			if place1 != "" {occupied += 1}
+			if place2 != "" {occupied += 1}
+			if place3 != "" {occupied += 1}
+			if place4 != "" {occupied += 1}
+			if place5 != "" {occupied += 1}
+
+			fmt.Printf("Осталось свободных мест: %d", 5-occupied)
+			fmt.Printf("Всего человек в очереди: %d", occupied)
+			continue
+		}
+
+		if input == "конец"{
+			fmt.Println(1, place1)
+			fmt.Println(2, place2)
+			fmt.Println(3, place3)
+			fmt.Println(4, place4)
+			fmt.Println(5, place5)
+			return
 		}
 
 
