375fb45dd2cfcf5a18cec012702b402112e62f11
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-07 22:38:33 +0700

    create square test

---
+++ Level 2. Функции, ошибки/Step 2. Функции. Правильное разделение кода на функции/Тестовые задания/Квадратные уравнения/square.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func SquareRoots(a, b, c float64) (float64, float64) {
+	if a == 0 && b == 0 {
+		return 0, 0
+	}
+
+	D := math.Pow(b, 2) - 4*a*c
+	if D < 0 {
+		return 0, 0
+	}
+
+	squarePlus := (-b + math.Sqrt(D)) / (2 * a)
+	square := (-b - math.Sqrt(D)) / (2 * a)
+	return square, squarePlus
+}
+
+func findDiscriminant(a, b, c float64) float64 {
+	D := math.Pow(b, 2) - 4*a*c
+	return D
+}
+
+func main() {
+	fmt.Println(SquareRoots(10, 4, 2))
+	fmt.Println(findDiscriminant(10, 4, 2))
+}
