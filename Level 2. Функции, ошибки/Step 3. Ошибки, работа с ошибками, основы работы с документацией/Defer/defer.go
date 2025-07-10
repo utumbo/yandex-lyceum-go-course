42c2ceb10c2b488d7108648d0ac4a1de2cb3bc59
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-11 01:43:42 +0700

    update defer.go

--- Level 2. Функции, ошибки/Step 3. Ошибки, работа с ошибками, основы работы с документацией/Defer/defer.go
+++ Level 2. Функции, ошибки/Step 3. Ошибки, работа с ошибками, основы работы с документацией/Defer/defer.go
@@ -1,27 +1,8 @@
 package main
 
-import "os"
+import "fmt"
 
-func createFile(filename string, data []byte) error {
-	f, err := os.Create(filename) // Создание файла с помощью покета os
-	if err != nil {
-		return err
-	}
-
-	_, err = f.Write(data) // Запись данных в файл
-	if err != nil {
-		f.Close()
-		return err
-	}
-
-	err = f.Close() // Закрытие файла
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func main(){
-	
+func main() {
+    defer fmt.Println("World") // Отложенный вызов функции вывода в консоль
+    fmt.Println("Hello")
 }
