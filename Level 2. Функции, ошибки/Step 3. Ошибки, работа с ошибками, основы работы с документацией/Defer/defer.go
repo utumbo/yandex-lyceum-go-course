f307bc1f8b31c84ec1fa9872dc2b0f83bec4b89f
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-11 01:42:05 +0700

    update defer.go

--- Level 2. Функции, ошибки/Step 3. Ошибки, работа с ошибками, основы работы с документацией/Defer/defer.go
+++ Level 2. Функции, ошибки/Step 3. Ошибки, работа с ошибками, основы работы с документацией/Defer/defer.go
@@ -2,6 +2,26 @@ package main
 
 import "os"
 
+func createFile(filename string, data []byte) error {
+	f, err := os.Create(filename) // Создание файла с помощью покета os
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data) // Запись данных в файл
+	if err != nil {
+		f.Close()
+		return err
+	}
+
+	err = f.Close() // Закрытие файла
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main(){
 	
 }
