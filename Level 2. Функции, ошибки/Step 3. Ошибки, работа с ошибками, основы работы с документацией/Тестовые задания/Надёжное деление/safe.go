4450ab1303f4a4faff8c2b32a7b4833c82e5ec60
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-12 02:29:30 +0700

    //

--- Level 2. Функции, ошибки/Step 3. Ошибки, работа с ошибками, основы работы с документацией/Тестовые задания/Надёжное деление/safe.go
+++ Level 2. Функции, ошибки/Step 3. Ошибки, работа с ошибками, основы работы с документацией/Тестовые задания/Надёжное деление/safe.go
@@ -1,6 +1,9 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var DivisionByZeroError = errors.New("division by zero is not allowed")
 
@@ -11,3 +14,20 @@ func Divide(a, b int) (float64, error) {
 	result := float64(a) / float64(b)
 	return result, nil
 }
+
+func main() {
+	fmt.Println(Divide(15, 2))
+	fmt.Println(Divide(100, 3))
+	fmt.Println(Divide(-227, 19))
+	fmt.Println(Divide(0, 15))
+	fmt.Println(Divide(1, 1))
+	fmt.Println(Divide(1, 0))
+}
+
+/*
+Напишите функцию Divide(a, b int) (float64, error),
+которая получает два целых числа и выводит результат их деления.
+Если делитель равен нулю, функция должна вернуть в качестве ответа 0 и
+сообщение об ошибке (division by zero is not allowed). Если второе число не равно нулю,
+верните в качестве ошибки nil. Ошибку нужно вынести в переменную: DivisionByZeroError.
+*/
